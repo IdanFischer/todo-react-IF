192aab1d5a57a6b2e69e12deb48a00b6e551fecb
Idan Fischer <Idanfischer5@gmail.com>
2023-02-18 18:42:43 -0500

    added updated done variable to true and false on click, and styling

--- src/components/TaskAdd.jsx
+++ src/components/TaskAdd.jsx
@@ -12,20 +12,24 @@ export default function TaskAdd({ tasks, setTasks }) {
       "done": false,
       "task": task
     }
-    
+    // this fetches the post api url
     fetch("https://todo-c9-api-if.web.app/tasks/", {
+      // sets the method as a post
       method: "POST",
+      // makes it so it reads it in json (a just in case type of code, not needed but always good for failsafe)
       headers: {
         "Content-Type": "application/json"
       },
+      // converts the new task into json format so that the api can receieve it
       body: JSON.stringify(newTask)
     })
+    // makes the response come back as json
     .then(res => res.json())
-    .then(setTasks)
+    // displays all the data and gets all
+    .then(data => setTasks(data))
+    // error handling 
     .catch(console.error)
   }
-  
-  console.log(`${task}`)
   
   return (
     <>
@@ -33,9 +37,11 @@ export default function TaskAdd({ tasks, setTasks }) {
         <Form.Group className="mb-3" controlId="formAddTask">
           <Form.Label>Add Task</Form.Label>
           <Form.Control 
+          // displays the task in the text bar so we and the computer can see
           value={task}
+          // gets setTask to change the task variable, and e.target.value hits the object target and the object value so that what gets displayed is what we are writing 
           onChange={e => setTask(e.target.value)}
-          type="task" 
+          type="text" 
           placeholder="Enter New Task" />
         </Form.Group>
         </Form>
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,15 +1,32 @@
 import { useEffect } from "react"
 import Button from "react-bootstrap/Button"
-import TaskAdd from "./TaskAdd"
 
 export default function TaskList({ tasks, setTasks }) {
 
+  // renders the list (once) and updates the list when a new one gets added
   useEffect(() => {
     fetch("https://todo-c9-api-if.web.app/tasks")
       .then(res => res.json())
+      // gets 
       .then(setTasks)
       .catch(console.error)
   }, [setTasks])
+
+  const toggleDone = (task) => {
+
+    const done = !!!task.done
+
+    fetch(`https://todo-c9-api-if.web.app/tasks/${task.taskId}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ done })
+    })
+      .then(res => res.json())
+      .then(setTasks)
+      .catch(console.error)
+  }
 
   return (
     <div>
@@ -18,10 +35,17 @@ export default function TaskList({ tasks, setTasks }) {
         !tasks
           ? <p>Loading...</p>
           : tasks.map(element => (
-            <Button key={element.taskId} className="col-12 display-flex mb-3">{element.task}</Button>
+            <Button key={element.taskId} variant="link" className={
+              element.done
+              ?
+              "false-done col-12 display-flex mb-3 variant=link "
+              :
+              "true-done col-12 display-flex mb-3"
+            }
+              onClick={() => toggleDone(element)}>{element.task}</Button>
           ))
       }
     </div>
   )
 
-}
+} 
